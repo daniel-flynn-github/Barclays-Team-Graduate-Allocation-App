045c3168e9a44cf38d017634891673370e65e19c
Gianmarco Cornacchia <2575307c@student.gla.ac.uk>
2022-11-18 17:02:38 +0000

    Added code to enable debug_toolbar, unsuccessful so far

--- django-backend/allocationproject/urls.py
+++ django-backend/allocationproject/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from allocationapp import views
@@ -21,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('allocation/', include('allocationapp.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
