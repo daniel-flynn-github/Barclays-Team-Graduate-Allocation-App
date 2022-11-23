e079cc594ca42a7d0a4bc48193420f8c0091cf4c
Luke Mullen <2542408m@student.gla.ac.uk>
2022-11-23 14:11:59 +0000

    JS vote incrementer is now working.

--- django-backend/static/js/vote_counter.js
+++ django-backend/static/js/vote_counter.js
@@ -1,14 +1,24 @@
-localStorage.voteCount = {};
+localStorage.setItem("voteCountsObject", "{}");
+const MAX_VOTES_PER_TEAM = 5;
 
 function countVotes(team_id) {
-    if (localStorage.voteCount[team_id] != undefined) {
-        console.log(localStorage.voteCount[team_id]);
-        localStorage.voteCount[team_id]++;
+    var voteCounts = JSON.parse(localStorage.getItem("voteCountsObject"));
+    console.log(JSON.stringify(voteCounts));
+
+    if (voteCounts[team_id] == null) {
+        voteCounts[[team_id]] = 1;
     } else {
-        console.log(localStorage.voteCount[team_id]);
-        localStorage.voteCount[team_id] = 1;
-        console.log(localStorage.voteCount[team_id]);
+        if (voteCounts[team_id] < MAX_VOTES_PER_TEAM) {
+            voteCounts[team_id]++;
+        } 
+        
+        if (voteCounts[team_id] == 5) {
+            document.getElementById("like_btn_team_" + team_id).classList.add("disabled");
+        }
     }
 
-    document.getElementById("voteCount_" + team_id).innerHTML = localStorage.voteCount.team_id;
+    document.getElementById("voteCount_" + team_id).innerHTML = voteCounts[team_id];
+
+    console.log(JSON.stringify(voteCounts));
+    localStorage.setItem("voteCountsObject", JSON.stringify(voteCounts));
 }
