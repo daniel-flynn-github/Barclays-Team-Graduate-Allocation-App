95db86f1a809996286832f9c6a78971fb2fd4fe4
李雨琦 <liyuqi@asavpnpriv151-38.cent.gla.ac.uk>
2023-01-25 12:17:55 +0000

    old changes

--- django-backend/allocationapp/models.py
+++ django-backend/allocationapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CustomUser(AbstractUser):
     # has ID PK, email, name, last name by default from AbstractUser
 
@@ -28,17 +29,20 @@ class Manager(models.Model):
     def __str__(self):
         return f"{self.user}"
 
+
 class Skill(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
         return f"Skill: {self.name}"
 
+
 class Technology(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
         return f"Technology: {self.name}"
+
 
 class Team(models.Model):
     name = models.CharField(max_length=128)
@@ -47,7 +51,7 @@ class Team(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     manager = models.ForeignKey(Manager, on_delete=models.DO_NOTHING)
     skills = models.ManyToManyField(Skill)
-    technologies = models.ManyToManyRel(Technology)
+    technologies = models.ManyToManyField(Technology)
 
     class Meta():
         verbose_name_plural = "Teams"
@@ -66,6 +70,7 @@ class Graduate(models.Model):
     def __str__(self):
         return f"{self.user}"
 
+
 class Admin(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
@@ -75,10 +80,11 @@ class Admin(models.Model):
     def __str__(self):
         return f"{self.user}"
 
+
 class Preference(models.Model):
     grad = models.ForeignKey(Graduate, on_delete=models.DO_NOTHING)
     team = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
     weight = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
 
     def __str__(self):
-        return f"Grad: {self.grad.user.email} has a preference of {self.weight} for {self.team.name}"    
+        return f"Grad: {self.grad.user.email} has a preference of {self.weight} for {self.team.name}"
