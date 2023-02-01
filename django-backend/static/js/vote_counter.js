192ca673cc6dd501a2a7e2e2cdb7ccb6cf1b4ab8
Luke Mullen <2542408m@student.gla.ac.uk>
2023-02-01 12:15:30 +0000

    Reset button JS now functional.

--- django-backend/static/js/vote_counter.js
+++ django-backend/static/js/vote_counter.js
@@ -36,3 +36,20 @@ function removeVotes(team_id) {
     document.getElementById("voteCount_" + team_id).innerHTML = voteCounts[team_id];
     localStorage.setItem("voteCountsObject", JSON.stringify(voteCounts));
 }
+
+function resetVotes() {
+    // When button is clicked:
+    //  1. Set votes for each team back to 0
+    //  2. Remove the red cross (if its there)
+    //  3. Ensure the thumbs up button is there and useable
+
+    var voteCounts = JSON.parse(localStorage.getItem("voteCountsObject"));
+    for (const [team_id, votes] of Object.entries(voteCounts)) {
+        document.getElementById("voteCount_" + team_id).innerHTML = 0;
+        document.getElementById("like_btn_team_" + team_id).classList.remove("disabled");
+        document.getElementById("remove_vote_team_" + team_id).hidden = true;
+    }
+
+    // Reset the JSON object for votes back to 0
+    localStorage.setItem("voteCountsObject", "{}");
+}
