2cc817954a78664561e9d43600df26f20d604aa5
Luke <2542408M@student.gla.ac.uk>
2023-02-15 11:57:13 +0000

    Make URLs more readable.

--- django-backend/allocationapp/urls.py
+++ django-backend/allocationapp/urls.py
@@ -6,19 +6,25 @@ app_name = 'allocationapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cast_votes/', views.cast_votes, name='cast_votes'),
-    path('vote_submitted/', views.vote_submitted, name="vote_submitted"),
-    path('result_page/', views.result_page, name="result_page"),
-    path('get_allocation/', views.get_allocation, name="get_allocation"),
-    path('manager_view_teams/', views.manager_view_teams, name='manager_view_teams'),
-    path('edit_team/<int:team_id>/', views.manager_edit_team, name='manager_edit_team'),
-    path('delete_team_member/<int:user_id>/', views.delete_team_member, name='delete_team_member'),
-    path('create_new_team/',views.create_new_team, name = 'create_new_team'),
-    path('create_new_grad/', views.create_new_grad, name = 'create_new_grad'),
-    path('upload/', views.upload_file, name='upload'),
-    path('upload/create/', views.populate_db, name = 'create'),
-    path('upload/reset/', views.reset_graduates_managers_view, name = 'reset'),
-    path('team_upload/', views.team_upload_file, name='team_upload'),
-    path('team_upload/create/', views.team_populate_db, name = 'team_create'),
-    path('team_upload/reset/', views.reset_teams_view, name = 'team_reset')
+
+    # Graduate URLs
+    path('graduate/cast_votes/', views.cast_votes, name='cast_votes'),
+    path('graduate/vote_submitted/', views.vote_submitted, name="vote_submitted"),
+    path('graduate/result_page/', views.result_page, name="result_page"),
+
+    # Manager URLs
+    path('manager/view_teams/', views.manager_view_teams, name='manager_view_teams'),
+    path('manager/edit_team/<int:team_id>/', views.manager_edit_team, name='manager_edit_team'),
+    path('manager/delete_team_member/<int:user_id>/', views.delete_team_member, name='delete_team_member'),
+
+    # Admin URLs
+    path('admin/create_new_team/',views.create_new_team, name = 'create_new_team'),
+    path('admin/create_new_grad/', views.create_new_grad, name = 'create_new_grad'),
+    path('admin/user_upload/', views.upload_file, name='upload'),
+    path('admin/user_upload/create/', views.populate_db, name = 'create'),
+    path('admin/user_upload/reset/', views.reset_graduates_managers_view, name = 'reset'),
+    path('admin/team_upload/', views.team_upload_file, name='team_upload'),
+    path('admin/team_upload/create/', views.team_populate_db, name = 'team_create'),
+    path('admin/team_upload/reset/', views.reset_teams_view, name = 'team_reset'),
+    path('admin/run_allocation/', views.get_allocation, name="get_allocation"),
 ]
