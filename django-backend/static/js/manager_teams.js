ea63713ec04b88034af6429cafe5119aae04d2a2
Luke <2542408M@student.gla.ac.uk>
2023-02-27 16:57:57 +0000

    Fix the AJAX delete button for manager view.

--- django-backend/static/js/manager_teams.js
+++ django-backend/static/js/manager_teams.js
@@ -1,26 +1,33 @@
-var gradRows = document.querySelectorAll(".row");
+var gradRows = document.querySelectorAll(".grads_in_team");
 
 gradRows.forEach(function(gradRow) {
     gradRow.addEventListener("click", function(event) {
-        var gradRowId = gradRow.getAttribute("id");
+        var gradRowId = gradRow.getAttribute("id");  // gets something like "grad_7"
 
         if (event.target.classList.contains("delete-btn")) {
+            // When the delete (X) button, within the graduate's row, is clicked
             event.preventDefault();
 
-            var deleteBtn = event.target;
-            deleteBtn.disabled = true;
-
             var xhr = new XMLHttpRequest();
-            xhr.open("GET", event.target.href);
+            xhr.open("GET", event.target.href);  // delete the graduate e.g. /delete_team_member/7/
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4) {
-                    deleteBtn.disabled = false;
                     if (xhr.status === 200) {
                         var response = JSON.parse(xhr.responseText);
                         if (response.success) {
-                            // 删除成功，移除对应的 HTML 元素
+                            // Remove the <li> containing the grad dynamically
                             var element = document.getElementById(gradRowId);
-                            element.parentNode.removeChild(element);
+                            var grad_id = gradRowId.substring(5);
+                            var grad_text = $("#grad_text_" + grad_id).text();
+                            element.parentNode.parentNode.removeChild(element.parentNode);
+
+                            // Then want to add this graduate back to the drop-down
+                            $("#select_graduate").append($("<option>", {
+                                value: grad_id,
+                                text: grad_text,
+                            }));
+                            $("#add_grad_form").attr('hidden', false);
+
                         } else {
                             alert("Failed to delete team member. Please wait seconds and try again.");
                         }
