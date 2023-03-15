475b4794576c58062db2ec6af9e3cdb3dbc47ab1
Luke Mullen <2542408m@student.gla.ac.uk>
2023-03-15 14:21:38 +0000

    Fix the bug of dropdown arrows staying down when another team is clicked.

--- django-backend/static/js/dropdown_icons.js
+++ django-backend/static/js/dropdown_icons.js
@@ -4,6 +4,15 @@ const teamButtons = document.querySelectorAll('.team_name_btn');
 // Loop through the buttons and add event listeners
 teamButtons.forEach(button => {
   button.addEventListener('click', () => {
+    // Ensure all icons for all other teams are UP
+    teamButtons.forEach(btn => {
+      if (btn !== button) {
+        const icon_btn = btn.querySelector('.bi');
+        icon_btn.classList.add('bi-chevron-down');
+        icon_btn.classList.remove('bi-chevron-up');
+      }
+    });
+
     // Get the icon element
     const icon = button.querySelector('.bi');
     // Toggle the icon class based on whether the accordion is expanded or collapsed
